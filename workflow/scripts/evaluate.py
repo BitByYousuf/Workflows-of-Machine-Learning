a557917f8ca0114b61d9c22fb105412fde88811b
AwkNinja <asadahsan420@gmail.com>
2025-05-08 04:12:29 +0200

    added evaluate.py

---
+++ workflow/scripts/evaluate.py
@@ -0,0 +1,4 @@
+# 1. it should take a file that has predictions
+# 2. uses different evaluation metrics to evaluate the model like precision, recall, f1 score etc
+# 3. save the metrics in a file
+# 4. it should also create some visualizations (probably makes more sense to make another file called visualize.py for this idk will see later)
